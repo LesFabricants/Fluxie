41d5900dd469840fc15f71cb4745d2712dfd3da8
Anthony Lenglet <Anthony.Lenglet@hotmail.com>
2026-03-24 16:46:18 +0100

    fix: export the plugins and types directly from the index file (#35)

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { Store } from "./store";
 export { FLUXIE_CONFIG } from "./config";
+export { cachePlugin } from "./plugins/cache";
+export { reduxDevtoolsPlugin } from "./plugins/redux-devtool";
+export { StoreOptions, StorePlugin } from "./type";
